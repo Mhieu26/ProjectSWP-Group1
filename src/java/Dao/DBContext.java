574b95726d48ad95ea11e8dd8c414726e333a493
Duong Thanh Dat <datdt1004@gmail.com>
2024-01-18 12:34:53 +0700

    feat: create package dao and ad dbcontext

---
+++ src/java/Dao/DBContext.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBContext {
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "root",
+                    password = "tao0noidauthenhe",
+                    url = "jdbc:mysql://localhost:3306/swp391";
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
